97de8908322b582d2bcdcc3dc066f66f07493891
SpecialTheK <Christian.Heideman@gmail.com>
2018-04-04 12:03:22 +0200

    Sende Hallo Jonas... ganz oft

--- Client/main.c
+++ Client/main.c
@@ -12,10 +12,11 @@
 
 int main(int argc, char **argv) {
     
-    int sock_fd;
+    int sock_fd, index;
     struct sockaddr_in srv_addr, cli_addr;
 
-    char out[] = "Hallo Jonas!\0";
+    char out[] = "HSOSSTP_INITX;512;myfile.txt\0";
+    char in[256];
     size_t out_len = strlen(out);
 
     if((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
@@ -27,7 +28,8 @@ int main(int argc, char **argv) {
     memset ((void *)&cli_addr, '\0', sizeof(cli_addr)); 
 	cli_addr.sin_family = AF_INET; 
 	cli_addr.sin_addr.s_addr = htonl(INADDR_ANY); // NOLINT
-	if (bind(sock_fd, (struct sockaddr *)&cli_addr, sizeof(cli_addr))<0){ 
+	if (bind(sock_fd, (struct sockaddr *)&cli_addr, sizeof(cli_addr))<0)
+    { 
         printf("Konnte Socket binden!\n");
         exit(1);
     } 
@@ -37,12 +39,23 @@ int main(int argc, char **argv) {
 	srv_addr.sin_addr.s_addr = inet_addr(argv[1]); 
 	srv_addr.sin_port = htons(SRV_PORT); // NOLINT
 
-    if(sendto(sock_fd, out, out_len, 0, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) != out_len)
+    for(;;)
     {
-        printf("Fehler beim Schreiben des Sockets!\n");
-        exit(1);
-    }
+        if(sendto(sock_fd, out, out_len, 0, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) != out_len)
+        {
+            printf("Fehler beim Schreiben des Sockets!\n");
+            exit(1);
+        }
 
+        if((recvfrom (sock_fd, in, MAXLINE, 0, (struct sockaddr*) NULL, (int*) NULL)) < 0); 
+		{ 
+			printf("Fehler beim Lesen des Sockets!\n");
+            exit(1);
+		} 
+
+        printf("ANS: %s\n", in);
+    }
+    
     printf("Daten gesendet!\n");
 
 	close (sock_fd); 
