9c5d7b6b878bbd6cecdc318197481233ae59425f
SpecialTheK <Christian.Heideman@gmail.com>
2018-04-09 09:44:33 +0200

    Client for sending test data

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,2 +1,37 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Client {
+
+	public static void main(String args[]) {
+
+		String msg = "Test Data";
+		DatagramSocket cSocket;
+		
+		byte[] sendData = new byte[256];
+		
+		try {
+			InetAddress ipAddr = InetAddress.getByName("127.0.0.1");
+			cSocket = new DatagramSocket();
+			
+			sendData = msg.getBytes();
+			
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipAddr, 8999);
+			cSocket.send(sendPacket);
+			
+			cSocket.close();
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
